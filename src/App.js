8dba3e5257f4af1d0f8836fdcf75f517689a4ab2
Yevheniia Prytkova <zheka.prytkova91@gmail.com>
2025-11-01 16:18:39 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -253,7 +253,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res =
-          await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}
+          await fetch(`https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}
     `);
         const data = await res.json();
         setMovie(data);
